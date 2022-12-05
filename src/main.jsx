5a7fab25b80c77988dcd4ea593fb972769296322
Seytkhan Balikbaev <seytkhanbalikbaev@Seytkhan.local>
2022-12-05 18:53:16 +0600

    fixed auth0

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 import { ProductsProvider } from "./context/products_context";
 import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
-import { UserProvider, useUserContext } from "./context/user_context";
+import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
